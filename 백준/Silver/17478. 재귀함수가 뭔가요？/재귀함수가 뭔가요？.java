3d2a47d2127d7d8879385aa6d3baf4063265fd06
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-02-05 14:58:17 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 152 ms, Memory: 15732 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -6,15 +6,14 @@ public class Main {
 	
 	static final String TEXT= "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.";
 	
-	static final String RF_HEADER_TEXT = "\"재귀함수가 뭔가요?\"";
-	
-	static final String RF_BODY_TEXT1 = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.";
-	static final String RF_BODY_TEXT2 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.";
-	static final String RF_BODY_TEXT3 = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
-	
-	static final String RF_FOOTER_TEXT = "라고 답변하였지.";
-	
-	static final String RF_STOP_TEXT = "\"재귀함수는 자기 자신을 호출하는 함수라네\"";
+	static final String[] RF_TEXT = {
+			"%s\"재귀함수가 뭔가요?\"\n",
+			"%s\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n",
+			"%s마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n",
+			"%s그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n",
+			"%s\"재귀함수는 자기 자신을 호출하는 함수라네\"\n",
+			"%s라고 답변하였지.\n"
+			};
 	
 	static final String RF_COUNT_CHECK = "____";
 	
@@ -28,43 +27,23 @@ public class Main {
 		
 		System.out.println(TEXT);
 		
-		rf(0);
+		rf(0,"");
 	}
 	
-
-	public static void rf(int count) {
+	private static void rf(int count, String underbar) {
 		
-		printRFCount(count);
+		System.out.printf(RF_TEXT[0],underbar);
 		
-		System.out.println(RF_HEADER_TEXT);//재귀함수가 뭔가요
-		
-		/**
-		 * 잘 들어보게... or 재귀함수는 자기자신을...
-		 */
 		if(count >= N) {
-			printRFCount(count);
-			System.out.println(RF_STOP_TEXT);
-			printRFCount(count);
-			System.out.println(RF_FOOTER_TEXT);
-			return;
+			System.out.printf(RF_TEXT[4],underbar);
 		}
 		else {
-			printRFCount(count);
-			System.out.println(RF_BODY_TEXT1);
-			printRFCount(count);
-			System.out.println(RF_BODY_TEXT2);
-			printRFCount(count);
-			System.out.println(RF_BODY_TEXT3);
+			System.out.printf(RF_TEXT[1],underbar);
+			System.out.printf(RF_TEXT[2],underbar);
+			System.out.printf(RF_TEXT[3],underbar);
+			rf(count+1,underbar+RF_COUNT_CHECK);
 		}
-		rf(count+1);
 		
-		printRFCount(count);
-		System.out.println(RF_FOOTER_TEXT);
-	}
-	
-	private static void printRFCount(int count) {
-		for(int i=0; i<count; i++) {
-			System.out.print(RF_COUNT_CHECK);
-		}
+		System.out.printf(RF_TEXT[5],underbar);
 	}
 }
