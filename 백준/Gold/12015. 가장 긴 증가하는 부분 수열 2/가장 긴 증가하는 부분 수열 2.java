9627487142f9e1cfda7dca42364eccfadda60836
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-02-19 16:51:01 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 492 ms, Memory: 96264 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,65 +1,58 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
-    static int[] lis;
-    static int lisPointer;
+    static int[] lis, arr;
+    static int lisPointer = 1;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-
-        int[] arr = new int[N];
-        lis = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
+
+        arr = new int[N+1];
+        lis = new int[N+1];
+
+        for(int i=1; i<=N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        lis[0] = arr[0];
+        lis[lisPointer] = arr[1];
 
-
-        lisPointer=0;
-        for(int i=1; i<N; i++){
+        for(int i=2; i<=N; i++){
             if(lis[lisPointer] < arr[i]){
-                lisPointer++;
+                lisPointer ++;
                 lis[lisPointer] = arr[i];
             }
-            else if(lis[lisPointer] >= arr[i]){
-                binarySearch(lisPointer, arr[i]);
+            else{
+                binarySearch(arr[i]);
             }
         }
 
+        System.out.println(lisPointer);
 
-        bw.write(lisPointer+1+"");
-        bw.flush();
-        bw.close();
-        br.close();
     }//main
 
-    private static void binarySearch(int j, int target){
-
+    private static void binarySearch(int target){
         int start = 0;
-        int end = j;
+        int end = lisPointer;
         int mid;
 
         while(start<=end){
-            mid  = (start+end)/2;
+            mid = (start+end)/2;
 
             if(lis[mid] == target){
                 lis[mid] = target;
                 return;
             }
-            else if(lis[mid] < target){
-                start = mid+1;
-            }
             else if(lis[mid] > target){
-                end = mid-1;
+                end = mid -1;
+            }
+            else if(lis[mid] < target){
+                start = mid + 1;
             }
         }
 
