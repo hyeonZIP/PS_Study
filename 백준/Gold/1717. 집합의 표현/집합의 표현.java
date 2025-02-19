9d77d2809151a7df8c46201840c5f4f027041d1a
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-02-19 10:15:47 +0900

    [Gold V] Title: 집합의 표현, Time: 404 ms, Memory: 50392 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int unionOrFind = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(unionOrFind == 1){
+                if(find(a)==find(b)){
+                    bw.write("YES\n");
+                }
+                else{
+                    bw.write("NO\n");
+                }
+            }
+            else{
+                union(a,b);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void union(int a, int b) {
+        int n1 = find(a);
+        int n2 = find(b);
+
+        if (n1 != n2) arr[n1] = n2;
+    }
+
+    private static int find(int n) {
+        if (arr[n] == n) return n;
+        else {
+            arr[n] = find(arr[n]);
+            return arr[n];
+        }
+    }
+}
