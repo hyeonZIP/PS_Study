58822fef3100b1fae9e369ba3132f0686aa097e1
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-03-14 15:27:22 +0900

    [Gold III] Title: 연구소 3, Time: 336 ms, Memory: 38296 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,16 +21,14 @@ public class Main {
         }
     }
 
-    static int[] dy = {0,0,1,-1};
-    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0, 0, 1, -1};
+    static int[] dx = {1, -1, 0, 0};
 
     static final char VIRUS = '*';
     static final char WALL = '-';
     static final char EMPTY = '.';
 
-    static int count = 1;
-
-    static int N, M,answer = Integer.MAX_VALUE;
+    static int N, M, answer = Integer.MAX_VALUE;
     static char[][] map;
     static List<Virus> virus = new ArrayList<>();
 
@@ -48,7 +48,7 @@ public class Main {
 
                 int input = Integer.parseInt(st.nextToken());
 
-                switch(input){
+                switch (input) {
                     case 0:
                         map[y][x] = EMPTY;
                         break;
@@ -57,48 +57,51 @@ public class Main {
                         break;
                     case 2:
                         map[y][x] = VIRUS;
-                        virus.add(new Virus(y,x,0,0));
+                        virus.add(new Virus(y, x, 0, 0));
                         break;
                 }
 
             }
         }
-
-        selectActivatingVirus();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        if (selectActivatingVirus()) {
+            bw.write("-1");
+        } else {
+            bw.write(answer + "");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void selectActivatingVirus(){
+    private static boolean selectActivatingVirus() {
 
         ArrayDeque<Virus> stack = new ArrayDeque<>();
         combination(stack, 0);
 
-        if (answer == Integer.MAX_VALUE){
-            System.out.println(-1);
-        }else{
-            System.out.println(answer);
-        }
+        return answer == Integer.MAX_VALUE;
     }
 
-    private static void combination(ArrayDeque<Virus> stack, int start){
+    private static void combination(ArrayDeque<Virus> stack, int start) {
 
-        if (stack.size() == M){
+        if (stack.size() == M) {
             bfs(stack.clone());
             return;
         }
 
-        for (int i=start; i< virus.size(); i++){
+        for (int i = start; i < virus.size(); i++) {
             stack.push(virus.get(i));
-            combination(stack, i+1);
+            combination(stack, i + 1);
             stack.pop();
         }
     }
 
-    private static void bfs(ArrayDeque<Virus> queue){
+    private static void bfs(ArrayDeque<Virus> queue) {
         boolean[][] visited = new boolean[N][N];
 
         char[][] clone = new char[N][N];
 
-        for (int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             clone[i] = map[i].clone();
         }
 
@@ -110,34 +113,34 @@ public class Main {
         }
 //        System.out.println("일 때 최소 시간");
         int virusMoveCount = 0;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Virus v = queue.poll();
             int y = v.y;
             int x = v.x;
             int time = v.time;
             int virusBridge = v.virusBridge;
             boolean deactivateVirusIsMovedFlag = false;
-            for (int i=0; i<4; i++){
-                int py = y+dy[i];
-                int px = x+dx[i];
+            for (int i = 0; i < 4; i++) {
+                int py = y + dy[i];
+                int px = x + dx[i];
 
                 //연구소 밖, 중복 방문, 벽 예외 처리
-                if (isOutOfRange(py,px) || visited[py][px] || map[py][px] == WALL) continue;
+                if (isOutOfRange(py, px) || visited[py][px] || map[py][px] == WALL) continue;
 
                 deactivateVirusIsMovedFlag = true;//더이상 나아갈 수 없으면 false로 유지됨
                 clone[py][px] = 'X';
-                if (map[py][px] == VIRUS){
-                    queue.offer(new Virus(py,px,time+1,virusBridge+1));
-                }else{
-                    queue.offer(new Virus(py,px,time+1,0));
+                if (map[py][px] == VIRUS) {
+                    queue.offer(new Virus(py, px, time + 1, virusBridge + 1));
+                } else {
+                    queue.offer(new Virus(py, px, time + 1, 0));
                 }
                 visited[py][px] = true;
             }//for
-            if (!deactivateVirusIsMovedFlag && map[y][x] == VIRUS){
-                time-=virusBridge;
-                virusMoveCount = Math.max(virusMoveCount,time);
-            }else if (!deactivateVirusIsMovedFlag && map[y][x] == EMPTY){
-                virusMoveCount = Math.max(virusMoveCount,time);
+            if (!deactivateVirusIsMovedFlag && map[y][x] == VIRUS) {
+                time -= virusBridge;
+                virusMoveCount = Math.max(virusMoveCount, time);
+            } else if (!deactivateVirusIsMovedFlag && map[y][x] == EMPTY) {
+                virusMoveCount = Math.max(virusMoveCount, time);
             }
         }//while
 
@@ -145,25 +148,24 @@ public class Main {
 
 //        System.out.println("최소 시간 : " + virusMoveCount);
 
-        answer = Math.min(virusMoveCount,answer);
+        answer = Math.min(virusMoveCount, answer);
     }
 
     private static int isSpreadAll(char[][] clone, int virusMoveCount) {
-        for (int i=0; i<N; i++){
-            for (int j=0; j<N; j++){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
 //                System.out.print(clone[i][j] + " ");
-                if (clone[i][j] == '.'){
+                if (clone[i][j] == '.') {
 //                    System.out.println("다 못채워서 최소시간 연산 X");
                     virusMoveCount = Integer.MAX_VALUE;
                     return virusMoveCount;
                 }
             }
-//            System.out.println();
         }
         return virusMoveCount;
     }
 
-    private static boolean isOutOfRange(int y, int x){
-        return y<0 || x<0 || y>=N || x>=N;
+    private static boolean isOutOfRange(int y, int x) {
+        return y < 0 || x < 0 || y >= N || x >= N;
     }
 }
