d75797a5cb75d9bdf4704c291180178690ea5194
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-03-21 13:38:32 +0900

    [Platinum V] Title: 현대모비스 트럭 군집주행, Time: 1320 ms, Memory: 342668 KB -BaekjoonHub

---
+++ 백준/Platinum/31938. 현대모비스 트럭 군집주행/현대모비스 트럭 군집주행.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static public class Node {
+        int end;
+        long weight;
+
+        public Node(int end, long weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+    }
+
+    static ArrayList<ArrayList<Node>> adj = new ArrayList<>();
+    static int N, M;
+    static long[] answer, dist;
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        dijkstra();
+
+        long sum = 0L;
+        for (long l : answer) {
+            sum += l;
+        }
+        System.out.println(sum);
+    }
+
+    private static void dijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparing(o -> o.weight));
+        pq.offer(new Node(1, 0));
+
+        boolean[] visited = new boolean[N + 1];
+
+        dist = new long[N + 1];
+        Arrays.fill(dist, Long.MAX_VALUE);
+        dist[1] = 0;
+
+        while (!pq.isEmpty()) {
+
+            Node cur = pq.poll();
+
+            if (!visited[cur.end]) {
+                visited[cur.end] = true;
+
+                for (Node next : adj.get(cur.end)) {
+
+                    if (!visited[next.end] && dist[next.end] >= dist[cur.end] + next.weight) {
+                        //이동 가능한 노드로 트럭을 보내는 중복 순열을 구해서 모두 pq에 삽입
+                        //각각의 노드에 최소 1개의 트럭은 보내야 한다
+//
+//                        System.out.println("cur.weight = " + cur.weight);
+//                        System.out.println("next.weight = " + next.weight);
+                        answer[next.end] = (long) (cur.weight * 0.9) + next.weight;
+//                        System.out.println(Arrays.toString(answer));
+
+                        dist[next.end] = dist[cur.end] + next.weight;
+                        pq.offer(new Node(next.end, cur.weight + next.weight));
+                    }
+                }
+            }
+        }
+
+//        System.out.println(Arrays.toString(answer));
+    }
+
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        answer = new long[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            adj.get(a).add(new Node(b, c));
+            adj.get(b).add(new Node(a, c));
+        }
+    }
+}
+
+
+//4 4
+//1 2 100
+//2 3 1100
+//2 4 1100
+//3 4 10
+//
+//2480
