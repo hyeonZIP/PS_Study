25274ac61f9bc09af79f40009666545a8db1f837
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-03-24 13:25:36 +0900

    [Gold V] Title: 정보 상인 호석, Time: 1256 ms, Memory: 171468 KB -BaekjoonHub

---
+++ 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int Q;
+	static long answer;
+	static HashMap<String,PriorityQueue<Integer>> map = new HashMap<>();
+	
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Q = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i=0; i<Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int cmd = Integer.parseInt(st.nextToken());
+			String name = st.nextToken();
+			int k = Integer.parseInt(st.nextToken());
+			PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(o->-o));
+			
+			if(cmd == 1) {
+				for(int ii=0; ii<k; ii++) {
+					pq.offer(Integer.parseInt(st.nextToken()));
+				}
+				
+				if(map.containsKey(name)) {
+					PriorityQueue<Integer> updatedPq = map.get(name);
+					updatedPq.addAll(pq);
+					map.put(name, updatedPq);
+				}else {
+					map.put(name, pq);
+				}
+			} else {
+				if(map.containsKey(name)) {
+					pq = map.get(name);
+					
+					if(pq.isEmpty()) continue;
+					
+					if(k > pq.size()) k = pq.size();
+					
+					for(int ii=0; ii<k; ii++) {
+
+						answer += pq.poll();
+					}
+					
+					map.put(name, pq);
+				}
+				
+				
+			}
+		}
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(answer+"");
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+}
