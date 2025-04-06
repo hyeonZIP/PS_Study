cecc56593610b083c68f3b3359115574d9f6a861
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-04-06 22:52:53 +0900

    [Gold IV] Title: 가르침, Time: 136 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -3,17 +3,14 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static final char[] DEFAULT_WORD = {'a', 'n', 't', 'c', 'i'};
-    static ArrayList<Character> word = new ArrayList<>();
-    static char[][] toLearnWord;
-    static int N, K;
-    static int defaultBit;//DEFAULT 비트
-    static int newWordBit;//DEFAULT 비트를 제외한 새로운 단어의 비트
+    static int[] words;
+    static int N, K, answer;
+    static int visited;
 
     public static void main(String[] args) throws IOException {
         init();
@@ -22,45 +19,35 @@ public class Main {
 
         if (K < 5) {
             bw.write("0");
-        } else if (K-5 >= word.size()) {
-            bw.write(N+"");
         } else {
-            int canLearnWord = K - 5;
 
-            int answer = 0;
-
-            //배울 수 있는 단어만큼 부분집합 생성 후 완전탐색으로 최대값 갱신
-            for (int i = 0; i < (1 << word.size()); i++) {
-                int combineBit = 0;
-                if (Integer.bitCount(i) == canLearnWord) {
-                    for (int j = 0; j < word.size(); j++) {
-                        if ((i & (1 << j)) != 0) {
-                            combineBit |= (1 << word.get(j) - 'a');
-                        }
-                    }
-                }
-
-                //list의 단어 또는 DEFAULT_WORD의 단어에 없는 단어는 카운트 하지 않는다.
-
-                int maxTemp = N;
-
-                for (int j = 0; j < N; j++) {
-                    for (char c : toLearnWord[j]) {
-                        if ((combineBit & (1 << c - 'a')) == 0 && (defaultBit & (1 << c - 'a')) == 0) {
-                            maxTemp--;
-                            break;
-                        }
-                    }
-                }
-
-                answer = Math.max(answer, maxTemp);
-
-            }
+            dfs(0, 0);
 
             bw.write(answer + "");
         }
 
         bw.close();
+    }
+
+    private static void dfs(int depth, int index) {
+        if (depth == K - 5) {
+            int temp = 0;
+
+            for (int word : words) {
+                int check = visited | word;
+                if (check == visited) temp++;
+            }
+
+            answer = Math.max(answer, temp);
+            return;
+        }
+
+        for (int i = index; i < 26; i++) {
+            if ((visited & (1 << i)) > 0) continue;
+            visited = visited | 1 << i;
+            dfs(depth + 1, i + 1);
+            visited = visited ^ 1 << i;
+        }
     }
 
     private static void init() throws IOException {
@@ -70,27 +57,21 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        toLearnWord = new char[N][];
+        words = new int[N];
 
         for (int i = 0; i < 5; i++) {
-            defaultBit |= 1 << DEFAULT_WORD[i] - 'a';
+            visited |= 1 << DEFAULT_WORD[i] - 'a';
         }
 
         for (int i = 0; i < N; i++) {
-            char[] temp = br.readLine().toCharArray();
-            toLearnWord[i] = temp;
+            String temp = br.readLine();
 
-            for (char c : temp) {
-                if ((defaultBit & (1 << c - 'a')) == 0 && (newWordBit & (1 << c - 'a')) == 0) {//기본 문자가 아닌 새로운 문자인 경우만 체크
-                    word.add(c);
-                    newWordBit |= 1 << c - 'a';
-                }
+            char[] newWords = temp.substring(4, temp.length() - 4).toCharArray();
+
+            for (char newWord : newWords) {
+                words[i] |= 1 << newWord - 'a';
             }
         }
-
-//        System.out.println("word = " + word);
-
-//        System.out.println(Integer.bitCount(bit));
     }
 
 }
