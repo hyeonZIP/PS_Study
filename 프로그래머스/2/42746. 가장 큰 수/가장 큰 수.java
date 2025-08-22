d24fd93ebc0611dc1ca83a519e3119fc080ba00b
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-08-22 14:22:28 +0900

    [level 2] Title: 가장 큰 수, Time: 362.75 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        
+        String[] strs = new String[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++){
+            
+            strs[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(strs, (o1,o2) -> {
+            return (o2 + o1).compareTo(o1 + o2);
+        });
+        
+        if(strs[0].equals("0")){
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(String str : strs){
+            
+            sb.append(str);
+        }
+        
+        return sb.toString();
+    }
+}
