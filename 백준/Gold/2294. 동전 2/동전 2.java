29e27039f7524ad116fcd7cf9056be11a46f4972
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-09-12 00:31:23 +0900

    [Gold V] Title: 동전 2, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,41 +1,72 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static int n,k;
+    private static int[] dp, arr;
+    private static String answer;
+
+    public static void main(String[] args) throws IOException{
+
+        init();
+
+        solution();
+
+        print();
+    }
+
+    private static void solution(){
+
+        for(int i=1; i<=n; i++){
+
+            int coin = arr[i];
+            
+            if(coin > k) continue;
+
+            for(int j=1; j<=k; j++){
+
+                if(coin <= j){
+
+                    dp[j] = Math.min(dp[j], dp[j-coin]+1);
+                }
+            }
+        }
+
+        answer = String.valueOf(dp[k] == Integer.MAX_VALUE-1 ? -1 : dp[k]);
+    }
+
+    private static void print() throws IOException{
+
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(answer);
+        bw.close();
+    }
+
+    private static void init() throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n + 1];
-        int[] dp = new int[k + 1];
+        arr = new int[n+1];
+        dp = new int[k+1];
 
-        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        Arrays.fill(dp, Integer.MAX_VALUE-1);
 
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for(int i=1; i<=n; i++){
+
+            int coin = Integer.parseInt(br.readLine());
+
+            arr[i] = coin;
+
+            if(coin > k) continue;
+
+            dp[coin] = 1;// 5원 동전이 있으면 5원의 최소 갯수는 1이다.
         }
-        Arrays.sort(arr);
-        dp[0] = 0;
-        //첫번째 동전으로 K원을 맞출수 있는지 체크
-        for (int i = 1; i <= n; i++) {
-            for (int j = arr[i]; j <= k; j++) {
-                dp[j] = Math.min(dp[j], dp[j - arr[i]] + 1);
-            }
-        }
-        
-        bw.write((dp[k] == Integer.MAX_VALUE - 1 ? -1 : dp[k]) + "");
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
