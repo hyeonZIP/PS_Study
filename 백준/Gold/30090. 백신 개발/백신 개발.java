c426446753ba87b597f061231d0ad28cc26fcea6
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-18 23:47:58 +0900

    [Gold V] Title: 백신 개발, Time: 396 ms, Memory: 206180 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/30090. 백신 개발/백신 개발.java
+++ 백준/Gold/30090. 백신 개발/백신 개발.java
@@ -15,69 +15,45 @@ public class Main {
     }
 
     private static void sol() {
-        // AABB BBAA
-        // AA BBAA
-
-        // VIRUS RUST STAND
-        // VI RU STAND
-
         // 백트래킹
-
-        dfs(new ArrayList<>(), new boolean[N]);
+        dfs("", new boolean[N], 0);
     }
 
-    private static void deleteDuple(List<String> strings) {
-        // System.out.println(strings);
+    private static String deleteDuple(String leftString, String rightString) {
+        int maxSubString = Math.min(leftString.length(), rightString.length());
 
-        String leftString = strings.get(0);
+        for (int j = maxSubString; j > 0; j--) {
+            String rightSubString = rightString.substring(0, j);
+            String leftSubString = leftString.substring(leftString.length() - j, leftString.length());
 
-        for (int i = 1; i < strings.size(); i++) {
-            boolean flag = false;
-
-            String rightString = strings.get(i);
-
-            int maxSubString = Math.min(leftString.length(), rightString.length());
-
-            for (int j = maxSubString; j > 0; j--) {
-                String rightSubString = rightString.substring(0, j);
-                String leftSubString = leftString.substring(leftString.length() - j, leftString.length());
-
-                // System.out.println("rightString : " + rightSubString);
-                // System.out.println("leftString : " + leftSubString);
-
-                if (leftSubString.equals(rightSubString)) {
-                    leftString = leftString.substring(0, leftString.length() - j) + rightString;
-                    // System.out.println("중간 업데이트 : " + leftString);
-                    flag = true;
-                    break;
-                }
-            }
-
-            if (!flag) {
-                leftString += rightString;
+            if (leftSubString.equals(rightSubString)) {
+                return leftString.substring(0, leftString.length() - j) + rightString;
             }
         }
-        // System.out.println("final leftString : " + leftString);
-        // System.out.println();
-        answer = Math.min(answer, leftString.length());
+        return leftString + rightString;
     }
 
-    private static void dfs(List<String> strings, boolean[] visited) {
-        if (strings.size() == N) {
-            deleteDuple(strings);
+    private static void dfs(String leftString, boolean[] visited, int depth) {
+        if (leftString.length() >= answer) {
+            return;
+        }
+        if (depth == N) {
+            answer = Math.min(answer, leftString.length());
+            return;
         }
 
         for (int i = 0; i < N; i++) {
             if (visited[i]) {
                 continue;
             }
-
-            strings.add(list[i]);
             visited[i] = true;
 
-            dfs(strings, visited);
+            if (leftString.equals("")) {
+                dfs(list[i], visited, depth + 1);
+            } else {
+                dfs(deleteDuple(leftString, list[i]), visited, depth + 1);
+            }
 
-            strings.remove(strings.indexOf(list[i]));
             visited[i] = false;
         }
     }
