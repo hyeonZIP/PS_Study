0b8656e09411f1ef8ecc9a5e157e1e5225d5c0d7
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-12-03 14:28:33 +0900

    [Gold IV] Title: 우체국, Time: 796 ms, Memory: 81676 KB -BaekjoonHub

---
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static class PostOffice {
+        private int x;
+        private int a;
+
+        public PostOffice(int x, int a) {
+            this.x = x;
+            this.a = a;
+        }
+    }
+
+    private static PostOffice[] postOffices;
+    private static int N;
+    private static long total;
+    private static int answer;
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        sol();
+
+        answer();
+    }
+
+    private static void sol() {
+        Arrays.sort(postOffices, Comparator.comparingInt(o -> o.x));
+
+        long current = 0l;
+
+        for (PostOffice postOffice : postOffices) {
+            current += Long.valueOf(postOffice.a);
+
+            if ((total % 2l == 0l && current >= total / 2l) || (total % 2l != 0l && current >= (total + 1l) / 2l)) {
+                answer = postOffice.x;
+                return;
+            }
+        }
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        postOffices = new PostOffice[N];
+
+        for (int i = 0; i < N; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            postOffices[i] = new PostOffice(input[0], input[1]);
+            total += Long.valueOf(input[1]);
+        }
+    }
+
+    private static void answer() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(answer));
+        bw.close();
+    }
+}
