9798c9cf05d8885ddc2426b254d85b00228ce2be
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2026-02-26 15:37:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.73 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
 
 ### 성능 요약
 
-메모리: 72 MB, 시간: 8.61 ms
+메모리: 75.7 MB, 시간: 0.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 16일 15:15:37
+2026년 02월 26일 15:37:53
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,63 +2,43 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    
-    int[] parent;
-    int answer = Integer.MAX_VALUE;
+    List<Integer>[] adj;
+    int N;
+    int minDiff = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
-        
-        for(int i=0; i<wires.length; i++){
-            init(n);
-            
-            for(int j = 0; j<wires.length; j++){
-                if(i==j) continue;
-                
-                int a = wires[j][0];
-                int b = wires[j][1];
-                
-                if(find(a) != find(b)){
-                    union(a, b);
-                }
-            }
-            
-            HashMap<Integer,Integer> map = new HashMap<>();
-            
-            for(int j=1; j<=n; j++){
-                int a = find(j);
-                map.put(a,map.getOrDefault(a,0)+1);
-            }
-            
-            if(map.size() == 2){
-                ArrayList<Integer> list = new ArrayList<>(map.values());
-                answer = Math.min(answer, Math.abs(list.get(0)-list.get(1)));
-            }
+        adj = new ArrayList[n+1];
+        N = n;
+        for(int i=1; i<=n; i++){
+            adj[i] = new ArrayList<>();
         }
-
+        
+        for(int[] wire : wires){
+            adj[wire[0]].add(wire[1]);
+            adj[wire[1]].add(wire[0]);
+        }
+        
+        dfs(1,0);
+        
+        int answer = minDiff;
+        
         return answer;
     }
     
-    public void init(int n){
-        parent = new int[n+1];
+    public int dfs(int current, int previous){
+        int count = 1;
         
-        for(int i=1; i<=n; i++){
-            parent[i] = i;
+        for(int next : adj[current]){
+            if(next != previous){
+                int childCount = dfs(next, current);
+                
+                int diff = Math.abs(childCount-(N-childCount));
+                minDiff = Math.min(minDiff, diff);
+                
+                count += childCount;
+            }
         }
-    }
-    
-    public void union(int a, int b){
-        a = find(a);
-        b = find(b);
         
-        if(a < b){
-            parent[b] = a;
-        }else{
-            parent[a] = b;
-        }
-    }
-    
-    public int find(int a){
-        if(a == parent[a]) return a;
-        else return parent[a] = find(parent[a]);
+        return count;
     }
 }
